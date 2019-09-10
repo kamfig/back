b688a99dc60441115df98971210e0671c4bdaef7
kamfig <k.figarski@interia.pl>
2019-09-10 21:59:17 +0200

    add next oop - constructors

--- src/ProgramowanieObiektowe1.java
+++ src/ProgramowanieObiektowe1.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class ProgramowanieObiektowe1 {
     int a;
     static void  test(){
@@ -14,6 +16,18 @@ public class ProgramowanieObiektowe1 {
 
         System.out.println(abc.szerokosc);
         System.out.println(abc2.szerokosc);
+
+        new Pont();
+        Pont p = new Pont();
+        p.x=10;
+        p.y=20;
+
+        Pont p2 = new Pont(100,200);
+
+        System.out.println("P: x;y = "+p.x+" "+p.y);
+        System.out.println("P2: x;y = "+p2.x+" "+p2.y);
+
+
     }
 }
 
@@ -27,4 +41,20 @@ public class ProgramowanieObiektowe1 {
         void off(){
 
         }
+    }
+    class Pont{
+
+        Pont(){
+          //konstruktor domyslny z nadaniem wartosci domyslnych
+            System.out.println("Wywolano konstruktor domyslny p");
+            x = 20;
+            y = 20;
+        }
+        Pont(int pierwszzy, int drugi){
+            System.out.println("Konstruktor p2 z 2 parametrami int");
+            x = pierwszzy;
+            y=drugi;
+        }
+        int x;
+        int y;
     }
